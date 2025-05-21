cc1cdc96ee48f013cb3485ac0eb9852c3d51df53
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-05-21 19:46:41 +0300

    Add body parser

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -6,4 +6,14 @@ movieController.get('/create', (req, res) => {
     res.render('create'); 
 });
 
+movieController.post('/create', (req, res) => {
+    const newMovie = req.body;
+
+    // Save Movie
+
+    // Redirect to home page
+
+    res.end();
+});
+
 export default movieController;
