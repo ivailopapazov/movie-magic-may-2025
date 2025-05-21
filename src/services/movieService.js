898733f8b47a675bf6b4432df392db8664ac7b8b
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-05-21 20:59:14 +0300

    Search by year and genre

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -39,9 +39,18 @@ const movies = [
 export default {
     getAll(filter = {}) {
         let result = movies.slice();
-        
+
         if (filter.search) {
             result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+        }
+
+        if (filter.genre) {
+            // result = result.filter(movie => movie.genre.localeCompare(filter.genre, undefined, { sensitivity: 'accent' }) === 0)
+            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre.toLowerCase())
+        }
+
+        if (filter.year) {
+            result = result.filter(movie => movie.year === filter.year);
         }
 
         return result;
