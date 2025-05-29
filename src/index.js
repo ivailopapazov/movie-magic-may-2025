9d9e7b517a2751fe5068da504a2fdcd79aabc505
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-05-29 19:00:55 +0300

    Fix details page

--- src/index.js
+++ src/index.js
@@ -22,6 +22,11 @@ app.engine('hbs', handlebars.engine({
             return '★'.repeat(Math.floor(rating));
             // return '&#x2605;'.repeat(Math.floor(rating));
         }
+    },
+    // Allow handlebars to use prototyp methods and properties of the base mongoose document
+    runtimeOptions: {
+        allowProtoMethodsByDefault: true,
+        allowProtoPropertiesByDefault: true,
     }
 }));
 
